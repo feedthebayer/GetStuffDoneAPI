6e29c0ea839330a540ad14be7af3d5093a5901f3
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-04-28 19:02:41 -0500

    feat: add db seed

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'figaro'
 gem 'active_model_serializers'
+gem 'faker'
 
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create users
+10.times do
+  User.create!(
+    username: Faker::Internet.user_name,
+  )
+end
+users = User.all
+
+# Create lists
+80.times do
+  users.sample.lists.create!(
+    name: Faker::Lorem.sentence,
+  )
+end
+
+lists = List.all
+
+# Create todo items
+350.times do
+  lists.sample.items.create!(
+    description: Faker::Lorem.sentence,
+  )
+end
+items = Item.all
+
+puts "Created #{users.count} users"
+puts "Created #{lists.count} lists"
+puts "Created #{items.count} items"
